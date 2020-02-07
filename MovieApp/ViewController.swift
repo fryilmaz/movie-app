90506b50c2b274ce00e8ee7897a7b619e7bc116e
Faruk Yılmaz <farukyilmaz@thy.com>
2020-02-07 23:18:05 +0300

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Faruk on 7.02.2020.
+//  Copyright © 2020 Faruk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
